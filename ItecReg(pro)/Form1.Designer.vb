5feaf19413fc837c4c374c4cd69da6315d15e8c1
Tosby <o2jose43@gmail.com>
2017-06-19 12:28:52 +0300

    Completed the design of the splash screen

--- ItecReg(pro)/Form1.Designer.vb
+++ ItecReg(pro)/Form1.Designer.vb
@@ -22,9 +22,75 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.LblLevel = New System.Windows.Forms.Label()
+        Me.LblVersion = New System.Windows.Forms.Label()
+        Me.LblCopyright = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 193)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(517, 23)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'LblLevel
+        '
+        Me.LblLevel.AutoSize = True
+        Me.LblLevel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblLevel.Location = New System.Drawing.Point(10, 167)
+        Me.LblLevel.Name = "LblLevel"
+        Me.LblLevel.Size = New System.Drawing.Size(0, 24)
+        Me.LblLevel.TabIndex = 1
+        '
+        'LblVersion
+        '
+        Me.LblVersion.AutoSize = True
+        Me.LblVersion.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblVersion.Location = New System.Drawing.Point(399, 219)
+        Me.LblVersion.Name = "LblVersion"
+        Me.LblVersion.Size = New System.Drawing.Size(0, 24)
+        Me.LblVersion.TabIndex = 2
+        '
+        'LblCopyright
+        '
+        Me.LblCopyright.AutoSize = True
+        Me.LblCopyright.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblCopyright.Location = New System.Drawing.Point(10, 219)
+        Me.LblCopyright.Name = "LblCopyright"
+        Me.LblCopyright.Size = New System.Drawing.Size(0, 24)
+        Me.LblCopyright.TabIndex = 3
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(542, 262)
+        Me.Controls.Add(Me.LblCopyright)
+        Me.Controls.Add(Me.LblVersion)
+        Me.Controls.Add(Me.LblLevel)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents LblLevel As Label
+    Friend WithEvents LblVersion As Label
+    Friend WithEvents LblCopyright As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- ItecReg(pro)/Form1.vb
+++ ItecReg(pro)/Form1.vb
@@ -1,3 +1,38 @@
-﻿Public Class Form1
-
+﻿Public NotInheritable Class Form1
+    Private Sub Form1_Paint(ByVal sender As Object,
+     ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        Dim gp As New System.Drawing.Drawing2D.GraphicsPath
+        Dim intval As Integer
+        Dim cirOrec As Integer
+        cirOrec = 0
+        If intval = 0 Then
+            intval = 90
+        End If
+        Dim chgcorn As Integer = intval
+        If chgcorn Mod 10 <> 0 Then
+            chgcorn = chgcorn - (chgcorn Mod 10)
+        End If
+        Dim r1 As New Rectangle(0, Me.Height - chgcorn, chgcorn, chgcorn)
+        Dim r2 As New Rectangle(Me.Width - chgcorn + 1,
+                Me.Height - chgcorn, chgcorn, chgcorn)
+        'the upper Arc
+        gp.AddArc(0, 0, chgcorn, chgcorn, 180, 90)
+        gp.AddArc(Me.Width - chgcorn + 1, 0, chgcorn, chgcorn, 270, 90)
+        ' the Body
+        gp.AddRectangle(New Rectangle(0, chgcorn / 2, Me.Width, Me.Height - chgcorn))
+        'the lower Arc
+        gp.AddArc(r1, -270, 90)
+        gp.AddArc(r2, 360, 90)
+        Region = New Region(gp)
+        Me.LblCopyright.Text = "Copyright © " & Now.Year
+        Me.LblVersion.Text = "Version " & My.Application.Info.Version.ToString()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.ProgressBar1.Increment(1)
+        Me.LblLevel.Text = "Loading . . " & ProgressBar1.Value & "%"
+        If ProgressBar1.Value = 100 Then
+            Login.Show()
+            Me.Close()
+        End If
+    End Sub
 End Class
