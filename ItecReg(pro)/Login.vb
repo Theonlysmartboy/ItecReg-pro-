90235ce67d272e47daa9fad4629bcf4425bdeb25
Tosby <o2jose43@gmail.com>
2017-06-21 18:05:29 +0300

    Begun writting the controls and logic for login

--- ItecReg(pro)/Login.vb
+++ ItecReg(pro)/Login.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Login
+    Dim result As Integer
+    Private Sub SignIn_Click(sender As Object, e As EventArgs) Handles SignIn.Click
+        If TxtUname.Text = "" And TxtPwd.Text = "" Then
+            result = MessageBox.Show("All fields are required please try again", "Invalid Login", MessageBoxButtons.RetryCancel)
+            If result = DialogResult.Retry Then
+                Me.Show()
+                TxtUname.Focus()
+            End If
+        ElseIf TxtUname.Text = "" Then
+            result = MessageBox.Show("All fields are required please try again", "Invalid Login", MessageBoxButtons.RetryCancel)
+            If result = DialogResult.Retry Then
+                Me.Show()
+                TxtUname.Focus()
+            Else
+                Me.Close()
 
+            End If
+        End If
+    End Sub
 End Class
